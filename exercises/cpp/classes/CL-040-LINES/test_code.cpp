6f8626eb6d7bb58fab574fc282e4a17b1eb59b22
Eugene Kornykhin <kornevgen@gmail.com>
2020-03-20 23:10:01 +0300

    Warnings were fixed.

--- exercises/cpp/classes/CL-040-LINES/test_code.cpp
+++ exercises/cpp/classes/CL-040-LINES/test_code.cpp
@@ -15,28 +15,28 @@ int main()
     cout << std::boolalpha << "1 test (output text): " <<  test_for_output_text() << endl;
     cout << "2 test (test hasNext at end): " << test_call_hasnext_at_end() << endl;
     cout << "3 test (empty file: " << test_empty_file() << endl;
-    
+
     cout << "testing completed." << endl;
     return 0;
 }
 
 bool test_for_output_text()
 {
-    char * path ="test_1.txt";
+    const char * path = "test_1.txt";
     TextFileReader f(path);
     return ("1_line" == f.get()) && ("2_line" == f.get()) && (f.hasNext() == true);
 }
 
 bool test_call_hasnext_at_end()
 {
-    char * path = "test_2.txt";
+    const char * path = "test_2.txt";
     TextFileReader f(path);
     return ("1_line" == f.get()) && (f.hasNext() == false);
 }
 
 bool test_empty_file()
 {
-    char * path = "test_3.txt";
+    const char * path = "test_3.txt";
     TextFileReader f(path);
     return (f.hasNext() == false);
 }
